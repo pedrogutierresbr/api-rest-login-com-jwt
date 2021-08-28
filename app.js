544186057403c1ab36d64c87dd6b504f92ee3968
Pedro Gutierres <pedrogabriel1@hotmail.com>
2021-08-28 17:00:40 -0300

    conectado ao banco de dados local

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const userRouter = require("./routes/userRouter");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/loginJwt", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", () => console.log("Houve um erro"));
+db.once("open", () => console.log("Banco carregado"));
 
 app.use("/user", userRouter);
 
