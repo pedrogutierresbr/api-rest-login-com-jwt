48bdd6c0cd5e7ef2bb4c846e4512233cd56024a7
Pedro Gutierres <pedrogabriel1@hotmail.com>
2021-08-29 16:04:34 -0300

    adicionado criptografia a senha criado pelo usuario e criado condicional para evitar email duplicado

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,23 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 async function register(req, res) {
+    let salt = bcrypt.genSaltSync(14);
+
+    //condicional para que não tenha email duplicado
+    const selectedUser = await User.findOne({ email: req.body.email });
+    if (selectedUser) return res.status(400).send("Email já utilizado");
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: bcrypt.hashSync(req.body.password, salt),
     });
 
     try {
         const savedUser = await user.save();
         res.send(savedUser);
+        console.log(savedUser);
     } catch (error) {
         res.status(400).send(error);
     }
--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
     name: { type: String, required: true, minlength: 3, maxlength: 20 },
     email: { type: String, required: true, minlength: 3, maxlength: 30 },
-    password: { type: String, required: true, minlength: 6, maxlength: 30 },
+    password: { type: String, required: true, minlength: 6, maxlength: 100 },
     createdAt: { type: Date, default: Date.now },
 });
 
