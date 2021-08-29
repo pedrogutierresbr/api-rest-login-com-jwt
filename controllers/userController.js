aa0641aa8f1f1c8914bc9904e81fa80c5dd0ea87
Pedro Gutierres <pedrogabriel1@hotmail.com>
2021-08-29 18:50:07 -0300

    adicionado jwt para criar token do usuário

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,13 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 async function register(req, res) {
     let salt = bcrypt.genSaltSync(14);
 
     //condicional para que não tenha email duplicado
     const selectedUser = await User.findOne({ email: req.body.email });
-    if (selectedUser) return res.status(400).send("Email já utilizado");
+    if (selectedUser) return res.status(400).send("Email already exists");
 
     const user = new User({
         name: req.body.name,
@@ -23,9 +24,17 @@ async function register(req, res) {
     }
 }
 
-function login(req, res) {
-    console.log("login");
-    res.send("Login");
+async function login(req, res) {
+    const selectedUser = await User.findOne({ email: req.body.email });
+    if (!selectedUser) return res.status(400).send("Email or Password incorrect");
+
+    const passwordAndUserMatch = bcrypt.compareSync(req.body.password, selectedUser.password);
+    if (!passwordAndUserMatch) return res.status(400).send("Email or Password incorrect");
+
+    const token = jwt.sign({ _id: selectedUser._id }, process.env.TOKEN_SECRET);
+
+    res.header("authorization-token", token);
+    res.send("User logged");
 }
 
 module.exports = { register, login };
